5d850e5a9ea680e692b8107b0a1a2c228d528b6c
jhasandeep <skjsan11@gmail.com>
2023-04-28 14:38:20 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  // Write your code here.
+
+  <div className="bg-container">
+    <h1>Congratulations</h1>
+    <div className="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>SANDEEP KUMAR JHA</h1>
+      <p>Mahaveer Swami Institute of Technology</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
